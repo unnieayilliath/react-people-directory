1e26fd3ce497360a165cea57d86b62f9c4cf9647
Unnie Ayilliath <Unnikannan.Ayilliath@opcw.org>
2020-05-04 19:56:29 +0200

    added searchbox with full width

--- src/webparts/peopleFinder/components/PeopleFinder.tsx
+++ src/webparts/peopleFinder/components/PeopleFinder.tsx
@@ -2,24 +2,31 @@ import * as React from 'react';
 import styles from './PeopleFinder.module.scss';
 import { IPeopleFinderProps } from './IPeopleFinderProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-
+import { SearchBox, ISearchBoxStyles } from 'office-ui-fabric-react/lib/SearchBox';
 export default class PeopleFinder extends React.Component<IPeopleFinderProps, {}> {
   public render(): React.ReactElement<IPeopleFinderProps> {
+  const searchBoxStyles: Partial<ISearchBoxStyles> = { root: { width: "100%" } };
     return (
-      <div className={ styles.peopleFinder }>
+    /*   <div className={ styles.peopleFinder }>
         <div className={ styles.container }>
           <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+            <div className={ styles.column }> */
+            <SearchBox
+              styles={searchBoxStyles}
+              placeholder="Find Staff"
+              onEscape={ev => {
+                console.log('Custom onEscape Called');
+              }}
+              onClear={ev => {
+                console.log('Custom onClear Called');
+              }}
+            onChange={newValue => console.log('SearchBox onChange fired: ' + newValue)}
+             onSearch={newValue => console.log('SearchBox onSearch fired: ' + newValue)}
+  />/* 
             </div>
           </div>
         </div>
-      </div>
+      </div> */
     );
   }
 }
